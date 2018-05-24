b5ba1593d6a81bbdb3fa50ae9e24486405591d3e
tobiaspreecha <craftyhorizonstudios@gmail.com>
2018-05-24 16:06:08 +0200

    1
    1

--- site_3/index.php
+++ site_3/index.php
@@ -1,5 +1,45 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<meta charset="utf-8">
-	<meta name="viewport content="didth
+		<meta charset="utf-8">
+		<meta name="viewport content="width=device-width">
+		<meta name="description" content="Self made portfolio web design">
+		<meta name="keywords" content="self made, portfolio, web design">
+		<meta name="author" content="Tobias Preecha">
+		
+		<link rel="stylesheet" href="./css/master.css">
+		
+		<title>Tobias Preecha | Portfolio</title>
+		
+	</head>
+	
+	<div id="body">
+		<body>
+			<div class="container">
+				<div id="header">
+					<header>
+						<div class="container">
+							<div id="branding">
+										<img src="">
+							</div>
+							<div id="siteNav">
+								<nav>
+									<ul>
+										<li><a href="index.php">Home</a></li>
+										<li><a href="index.php">Home</a></li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</header>
+				</div>
+				<div id="footer">
+					<footer>
+			
+					</footer>
+				</div>
+			</div>
+		</body>
+	</div>
+</html>
+	
