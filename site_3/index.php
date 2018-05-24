cf8351784f8fdcf68e31e9148a400e69898af751
tobiaspreecha <craftyhorizonstudios@gmail.com>
2018-05-24 16:26:57 +0200

    1

--- site_3/index.php
+++ site_3/index.php
@@ -26,16 +26,46 @@
 								<nav>
 									<ul>
 										<li><a href="index.php">Home</a></li>
-										<li><a href="index.php">Home</a></li>
+										<li><a href="index.php?contact">Contact</a></li>
+										<li><a href="index.php?creations">Creations</a></li>										
 									</ul>
 								</nav>
 							</div>
 						</div>
 					</header>
+					<div id="showcase">
+						<section>
+							<div class="container">
+								<h1>Tobias Preecha</h1>
+								<p>Bacon ipsum dolor amet cupim doner short ribs corned beef meatball hamburger turducken tenderloin prosciutto. Capicola sausage pork ribeye chuck flank bresaola beef, t-bone frankfurter fatback. Porchetta short loin pig ground round sausage ham shankle burgdoggen tri-tip rump tongue tail ribeye.</p>
+							</div>
+						</section>
+					</div>
+					<div id="newsletter">
+						<section>
+						<div class="container">
+							<h1>Subscribe To My Newsletter</h1>
+							<form>
+								<input type="email" placeholder="enter Email..">
+								<button type="submit" class="button_1">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<div id="boxes">
+						<div class="container">
+							<div class="box">
+								<h3>TicTacToe</h3>
+								<p>Spicy jalapeno bacon ipsum dolor amet jowl pancetta ham jerky t-bone short loin porchetta brisket ham hock landjaeger doner shoulder pork leberkas. Turkey salami buffalo, bresaola shankle shank kevin biltong pig alcatra swine.</p>
+								<h3>Empty</h3>
+								<p>Spicy jalapeno bacon ipsum dolor amet jowl pancetta ham jerky t-bone short loin porchetta brisket ham hock landjaeger doner shoulder pork leberkas. Turkey salami buffalo, bresaola shankle shank kevin biltong pig alcatra swine.</p>
+								<h3>Empty</h3>
+								<p>Spicy jalapeno bacon ipsum dolor amet jowl pancetta ham jerky t-bone short loin porchetta brisket ham hock landjaeger doner shoulder pork leberkas. Turkey salami buffalo, bresaola shankle shank kevin biltong pig alcatra swine.</p>
+						</div>
+					</div>
 				</div>
 				<div id="footer">
 					<footer>
-			
+						
 					</footer>
 				</div>
 			</div>
